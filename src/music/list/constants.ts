cf5c5affcf3d3b59b6a13cd623a0b2046d7b313a
callunaborealis <callunaborealis@gmail.com>
2021-03-14 16:44:26 +0800

    Rename mandatorySpace to whitespacePattern

--- src/music/list/constants.ts
+++ src/music/list/constants.ts
@@ -1,7 +1,10 @@
-import { prefixCommandTerminatorPatternStr } from '../../constants';
+import {
+  askingForPermissionPattern,
+  whitespacePattern,
+  prefixCommandTerminatorPatternStr,
+} from '../../constants';
 import { youtubeLinkPatternStr } from '../constants';
 
-const mandatorySpace = '[ ]{1,3}';
 const pageNumberPattern = '[\\d]+';
 /**
  * Aliases here should extend Groovy unless contradictions appear
@@ -29,10 +32,137 @@ export const showPlaylistPrefixCommandPatterns = [
       showPlaylistPrefixCommandPattern,
       ')',
       // To prevent matching with playlist
-      `(?!${mandatorySpace}${youtubeLinkPatternStr})`,
+      `(?!${whitespacePattern}${youtubeLinkPatternStr})`,
       // page 2 | 2 | none
-      `(?:${mandatorySpace}(?:(?:${pageTermsPattern})${mandatorySpace})?(${pageNumberPattern})(?:\\.[\\d]+)?)?`,
+      `(?:${whitespacePattern}(?:(?:${pageTermsPattern})${whitespacePattern})?(${pageNumberPattern})(?:\\.[\\d]+)?)?`,
       prefixCommandTerminatorPatternStr,
+    ].join(''),
+    'gim',
+  ),
+];
+
+// Natural Language Request processing
+
+const trackVariants = ['song', 'track'];
+const whatIsVariants = ['what is', "what\\'s"];
+const whatAreVariants = ['what are', "what're"];
+const rightNowVariants = ['right now', 'at the moment', 'now', 'here'];
+const whatPlayingRequests = [
+  // what is playing
+  `(?:${whatIsVariants.join(
+    '|',
+  )})${whitespacePattern}playing(?:${whitespacePattern}(?:${rightNowVariants.join(
+    '|',
+  )}))?`,
+  // what are you playing right now
+  `(?:${whatAreVariants.join(
+    '|',
+  )})${whitespacePattern}you${whitespacePattern}playing(?:${whitespacePattern}(?:${rightNowVariants.join(
+    '|',
+  )}))?`,
+  // what is this song
+  `(?:${whatIsVariants.join(
+    '|',
+  )})${whitespacePattern}this${whitespacePattern}(?:${trackVariants.join(
+    '|',
+  )})`,
+];
+const whatPlayingRequestPattern = whatPlayingRequests
+  .map((p) => `(?:${p})`)
+  .join('|');
+const showPlayingRequests = [
+  // show the playlist
+  `show${whitespacePattern}(?:the${whitespacePattern})?playlist`,
+  // show what is playing
+  `show${whitespacePattern}what${whitespacePattern}is${whitespacePattern}playing`,
+];
+const showPlayingRequestPattern = showPlayingRequests
+  .map((p) => `(?:${p})`)
+  .join('|');
+
+const pageNrPatterns = [
+  // page 2
+  `(?:(?:${pageTermsPattern})${whitespacePattern}(${pageNumberPattern}))`,
+  // 2nd page
+  `(?:(${pageNumberPattern})(?:st|nd|rd|th)${whitespacePattern}(?:${pageTermsPattern}))`,
+];
+const pageNrPatternGroup = pageNrPatterns.join('|');
+
+const showPageNrOfPlaylist = [
+  // show page 2 of this playlist
+  `show${whitespacePattern}(?:${pageNrPatternGroup}) of (?:the|this) playlist`,
+  // show this playlist page 2
+  `show${whitespacePattern}(?:(?:the|this)${whitespacePattern})?playlist${whitespacePattern}((?:${pageTermsPattern}) ${pageNumberPattern})`,
+  // show the page 2 of this playlist
+  `show${whitespacePattern}(?:the${whitespacePattern})?(?:${pageNrPatternGroup})${whitespacePattern}(?:of|for|in)${whitespacePattern}(?:(?:the|this)${whitespacePattern})?playlist `,
+];
+const showPageNrOfPlaylistPattern = showPageNrOfPlaylist
+  .map((p) => `(?:${p})`)
+  .join('|');
+
+// Show everything...
+const thatIsPlayingVariants = [
+  'playing',
+  'that is playing',
+  'that will play',
+  'that will be played',
+  'that is gonna play',
+];
+
+const showAllRequests = [
+  // show all of the playlist
+  `show${whitespacePattern}all${whitespacePattern}of${whitespacePattern}the${whitespacePattern}playlist`,
+  // show everything playing
+  `show${whitespacePattern}everything${whitespacePattern}(?:${thatIsPlayingVariants.join(
+    '|',
+  )}))`,
+  // show what is playing
+  `show${whitespacePattern}all${whitespacePattern}of${whitespacePattern}(?:${whatPlayingRequestPattern})`,
+  // show all the playlist tracks
+  `show${whitespacePattern}all${whitespacePattern}the${whitespacePattern}playlist${whitespacePattern}(?:${trackVariants.join(
+    '|',
+  )})`,
+];
+
+export const showPlaylistNaturalRequestPatterns = [
+  // what is playing / show playlist - no page number specified
+  new RegExp(
+    [
+      '(?:',
+      '(?:',
+      // optional "Are you able to"
+      `(?:${askingForPermissionPattern}${whitespacePattern})?`,
+      // "show the playlist / show what is playing"
+      `(?:(?:${whatPlayingRequestPattern})|(?:${showPlayingRequestPattern}))`,
+      ')',
+      ')',
+    ].join(''),
+    'gim',
+  ),
+  // what is playing / show playlist - no page number specified
+  new RegExp(
+    [
+      '(?:',
+      '(?:',
+      // optional "Are you able to"
+      `(?:${askingForPermissionPattern}${whitespacePattern})?`,
+      // "show the playlist / show what is playing"
+      `(?:(?:${whatPlayingRequestPattern})|(?:${showPlayingRequestPattern}))`,
+      ')',
+      ')',
+    ].join(''),
+    'gim',
+  ),
+  new RegExp(
+    [
+      '(?:',
+      '(?:',
+      // optional "Are you able to"
+      `(?:${askingForPermissionPattern}${whitespacePattern})?`,
+      // "show the playlist / show what is playing"
+      `(?:${showPageNrOfPlaylistPattern})?`,
+      ')',
+      ')',
     ].join(''),
     'gim',
   ),
