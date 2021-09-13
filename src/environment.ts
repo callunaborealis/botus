16a6e93ccf14be1a159e3cfafb5f977ad3c86e57
callunaborealis <callunaborealis@gmail.com>
2021-09-14 00:28:51 +0800

    Add discord app invite link as env var

--- src/environment.ts
+++ src/environment.ts
@@ -3,3 +3,5 @@ dotenv.config({ path: `${__dirname}/../.env` });
 
 export const SERVER_APP_PORT = process.env.SERVER_APP_PORT || 3000;
 export const DISCORD_APP_BOT_TOKEN = process.env.DISCORD_APP_BOT_TOKEN || '---';
+export const DISCORD_APP_INVITE_LINK =
+  process.env.DISCORD_APP_INVITE_LINK || '#';
